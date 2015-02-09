9b482914eafd2b75ba6a1bfa4187e3fc58359087
sasiprabha008@gmail.com <sasiprabha008@gmail.com>
2015-02-09 07:06:06 +0530

    Update #13

--- Basic/Model.js
+++ Basic/Model.js
@@ -1,3 +1,4 @@
 ﻿
 include("./Model/PurchaseOrderLine/PurchaseOrderLine-events.js");
 include("./Model/PurchaseOrderLine/PurchaseOrderLine-methods.js");
+include("./Model/ProductionOrder/ProductionOrder-methods.js");
---
+++ Basic/Model/ProductionOrder/ProductionOrder-methods.js
@@ -0,0 +1,14 @@
+﻿
+
+model.ProductionOrder.methods.generateFromPlannedSalesOrders = function() {
+	
+	var salesOrders = ds.SalesOrder.query("State == 'planned'");
+	var poList = [];
+	
+	salesOrders.distinctValues("Shop.Factory").forEach(function(factory){
+		poList.push(new ds.ProductionOrder({Factory:factory}));
+		ds.SalesOrderLine.query("");
+	});
+	salesOrders.
+	 
+};
