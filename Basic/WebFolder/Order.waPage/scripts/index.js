7ba7d64d3bc1045d561e96fe4f3c7903a6c3b7ca
sasiprabha008@gmail.com <sasiprabha008@gmail.com>
2015-02-11 01:37:37 +0530

    Update #18

--- Basic/WebFolder/Order.waPage/scripts/index.js
+++ Basic/WebFolder/Order.waPage/scripts/index.js
@@ -2,15 +2,58 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var id_btn_po_clear = {};	// @button
+	var id_btn_po_search = {};	// @button
 	var btn_clear = {};	// @button
 	var id_btn_search = {};	// @button
 // @endregion// @endlock
 
 // eventHandlers// @lock
 
+	id_btn_po_clear.click = function id_btn_po_clear_click (event)// @startlock
+	{// @endlock
+		$$('id_qry_po_date').clear();
+		$$('id_qry_po_shop').setValue('');
+		$$('id_qry_po_time').clear();
+		qry_po_time ='';
+		qry_po_date ='';
+		qry_po_shop ='';
+	};// @lock
+
+	id_btn_po_search.click = function id_btn_po_search_click (event)// @startlock
+	{// @endlock
+		//alert('aaa1'+ qry_so_date);
+		var query_array = [];
+		if(qry_po_time != ''){
+			
+			query_array.push('Time === '+qry_po_time);
+		}
+		if(qry_po_date){
+			
+			query_array.push('Date >= '+qry_po_date[0].toISOString()+' and Date <= '+qry_po_date[1].toISOString());
+		}
+		if(qry_po_shop){
+			
+			query_array.push('Factory.ID === '+qry_po_shop);
+		}
+		
+		var query_str = '';
+		for(i = 0;i<query_array.length;i++){
+			
+			query_str = query_str+(i!=0?'and ':'')+query_array[i];
+			
+		}
+		sources.productionOrder.query(query_str);
+	};// @lock
+
 	btn_clear.click = function btn_clear_click (event)// @startlock
 	{// @endlock
-		
+		$$('id_qry_so_date').clear();
+		$$('id_qry_so_shop').setValue('');
+		$$('id_qry_so_time').clear();
+		qry_so_time ='';
+		qry_so_date ='';
+		qry_so_shop ='';
 	};// @lock
 
 	id_btn_search.click = function id_btn_search_click (event)// @startlock
@@ -25,6 +68,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 			
 			query_array.push('Date >= '+qry_so_date[0].toISOString()+' and Date <= '+qry_so_date[1].toISOString());
 		}
+		if(qry_so_shop){
+			
+			query_array.push('Shop.ID === '+qry_so_shop);
+		}
 		
 		var query_str = '';
 		for(i = 0;i<query_array.length;i++){
@@ -37,6 +84,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 
 // @region eventManager// @startlock
+	WAF.addListener("id_btn_po_clear", "click", id_btn_po_clear.click, "WAF");
+	WAF.addListener("id_btn_po_search", "click", id_btn_po_search.click, "WAF");
 	WAF.addListener("btn_clear", "click", btn_clear.click, "WAF");
 	WAF.addListener("id_btn_search", "click", id_btn_search.click, "WAF");
 // @endregion
