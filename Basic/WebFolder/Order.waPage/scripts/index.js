3b9d0b3be1b69fbcea657c4db697467559e70a35
sasiprabha008@gmail.com <sasiprabha008@gmail.com>
2015-02-15 12:38:34 +0530

    Update #19

--- Basic/WebFolder/Order.waPage/scripts/index.js
+++ Basic/WebFolder/Order.waPage/scripts/index.js
@@ -2,6 +2,8 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var id_btn_ro_clear = {};	// @button
+	var id_btn_ro_search = {};	// @button
 	var id_btn_po_clear = {};	// @button
 	var id_btn_po_search = {};	// @button
 	var btn_clear = {};	// @button
@@ -9,6 +11,42 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	id_btn_ro_clear.click = function id_btn_ro_clear_click (event)// @startlock
+	{// @endlock
+		$$('id_qry_ro_date').clear();
+		$$('id_qry_ro_shop').setValue('');
+		$$('id_qry_ro_time').clear();
+		qry_ro_time ='';
+		qry_ro_date ='';
+		qry_ro_shop ='';
+	};// @lock
+
+	id_btn_ro_search.click = function id_btn_ro_search_click (event)// @startlock
+	{// @endlock
+		//alert('aaa1'+ qry_so_date);
+		var query_array = [];
+		if(qry_ro_time != ''){
+			
+			query_array.push('Time === '+qry_ro_time);
+		}
+		if(qry_ro_date){
+			
+			query_array.push('Date >= '+qry_ro_date[0].toISOString()+' and Date <= '+qry_ro_date[1].toISOString());
+		}
+		if(qry_ro_shop){
+			
+			query_array.push('Shop.ID === '+qry_ro_shop);
+		}
+		
+		var query_str = '';
+		for(i = 0;i<query_array.length;i++){
+			
+			query_str = query_str+(i!=0?'and ':'')+query_array[i];
+			
+		}
+		sources.returnOrder.query(query_str);
+	};// @lock
 
 	id_btn_po_clear.click = function id_btn_po_clear_click (event)// @startlock
 	{// @endlock
@@ -84,6 +122,8 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 
 // @region eventManager// @startlock
+	WAF.addListener("id_btn_ro_clear", "click", id_btn_ro_clear.click, "WAF");
+	WAF.addListener("id_btn_ro_search", "click", id_btn_ro_search.click, "WAF");
 	WAF.addListener("id_btn_po_clear", "click", id_btn_po_clear.click, "WAF");
 	WAF.addListener("id_btn_po_search", "click", id_btn_po_search.click, "WAF");
 	WAF.addListener("btn_clear", "click", btn_clear.click, "WAF");
