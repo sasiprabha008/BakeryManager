2933986c05e7a0d5baf25281bf3a79e2c776d534
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2015-03-06 14:39:33 +0530

    Update #24

--- Basic/WebFolder/AllOrders.waPage/scripts/index.js
+++ Basic/WebFolder/AllOrders.waPage/scripts/index.js
@@ -3,85 +3,137 @@
 
 WAF.onAfterInit = function onAfterInit() {// @lock
 	
-	function prepareTable(){
-		
-		var lines = sources.SalesOrder.salesOrderLineCollection;
-		for(i = 0;i<lines.length;i++){
-				
-		}
-		
+	function genPO() {
+
+	    var lines = [];
+	    //		for(i = 0;i<lines.length;i++){
+	    //				
+	    //		}
+	    sources.salesOrder.toArray("",{onSuccess: function(event)  // asynchronous call
+    { 
+        var myArray = event.result;
+        var display
+        for(i = 0;i<myArray.length;i++){
+        		
+        }
+    }});
 	}
 	
 	
-	function preparePrint(){
+	function prepareSOPrint() {
+		var SO = sources.salesOrder;
+		SO.Shop.load({
+    onSuccess: function(event) // only if there is a current entity in the person datasource
+    {
+        var Shop = event.entity.Name.value;
+        
+        var SOLine = [];
+	SOLine.push([{
+	                        text: '#',
+	                        style: 'tableHeader'
+	                    }, {
+	                        text: 'Item',
+	                        style: 'tableHeader'
+	                    }, {
+	                        text: 'Quantity',
+	                        style: 'tableHeader'
+	                    }]);
+
+	sources.salesOrderLine.getValues("'Item.Name','Quantity'","'Order.ID'===:1",{onSuccess:function(event) {
+		var LineNo = 1;
+		event.result.forEach(function(line){
+			SOLine.push([(LineNo++)+'', line.Item.Name, line.Quantity+'']);
+		});
+		
+		
+		
+		
 		var dd = {
-	content: [
-				
-								{ text: 'but you can provide a custom styler as well', margin: [0, 20, 0, 8] },
-								{
-						style: 'tableExample',
-						table: {
-								headerRows: 1,
-								widths: [100, '*', 200],
-								body: [
-										[{ text: 'Header 1', style: 'tableHeader' }, { text: 'Header 2', style: 'tableHeader'}, { text: 'Header 3', style: 'tableHeader' }],
-										[ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-										[ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-										[ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-										[ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-										[ 'Sample value 1', 'Sample value 2', 'Sample value 3' ],
-								]
-						},
-						layout: {
-														hLineWidth: function(i, node) {
-																return (i === 0 || i === node.table.body.length) ? 2 : 1;
-														},
-														vLineWidth: function(i, node) {
-																return (i === 0 || i === node.table.widths.length) ? 2 : 1;
-														},
-														hLineColor: function(i, node) {
-																return (i === 0 || i === node.table.body.length) ? 'black' : 'black';
-														},
-														vLineColor: function(i, node) {
-																return (i === 0 || i === node.table.widths.length) ? 'black' : 'gray';
-														},
-														// paddingLeft: function(i, node) { return 4; },
-														// paddingRight: function(i, node) { return 4; },
-														// paddingTop: function(i, node) { return 2; },
-														// paddingBottom: function(i, node) { return 2; }
-						}
-				}
-	],
-	styles: {
-		header: {
-			fontSize: 18,
-			bold: true,
-			margin: [0, 0, 0, 10]
-		},
-		subheader: {
-			fontSize: 16,
-			bold: true,
-			margin: [0, 10, 0, 5]
-		},
-		tableExample: {
-			margin: [0, 5, 0, 15]
-		},
-		tableHeader: {
-			bold: true,
-			fontSize: 13,
-			color: 'black'
-		}
-	},
-	defaultStyle: {
-		// alignment: 'justify'
-	}
+        content: [
+
+        {
+            text: 'Order No: ' + SO.OrderNo,
+            margin: [0, 0, 0, 0]
+        }, 
+        {
+            text: 'Shop : '+Shop,
+            margin: [0, 0, 0, 0]
+        }, 
+        {
+            text: 'Date : '+SO.Date.toLocaleDateString("en-GB"),
+            margin: [0, 0, 0, 0]
+        },
+        {
+            text: 'Time :' +SO.Time,
+            margin: [0, 0, 0, 0]
+        },
+        {
+            style: 'tableExample',
+            table: {
+                headerRows: 1,
+                widths: [10, '*', 200],
+                body: SOLine
+            },
+            layout: {
+                hLineWidth: function(i, node) {
+                    return (i <= 1 || i === node.table.body.length) ? 0.5 : 0.5;
+                },
+                vLineWidth: function(i, node) {
+                    return (i === 0 || i === node.table.widths.length) ? 0.5 : 0.5;
+                },
+                hLineColor: function(i, node) {
+                    return (i <= 1 || i === node.table.body.length) ? 'black' : 'gray';
+                },
+                vLineColor: function(i, node) {
+                    return (i === 0 || i === node.table.widths.length) ? 'black' : 'black';
+                },
+                // paddingLeft: function(i, node) { return 4; },
+                // paddingRight: function(i, node) { return 4; },
+                // paddingTop: function(i, node) { return 2; },
+                // paddingBottom: function(i, node) { return 2; }
+            }
+        }],
+        styles: {
+            header: {
+                fontSize: 12,
+                bold: true,
+                margin: [0, 0, 0, 10]
+            },
+            subheader: {
+                fontSize: 12,
+                bold: true,
+                margin: [0, 10, 0, 5]
+            },
+            tableExample: {
+                margin: [0, 5, 0, 15]
+            },
+            tableHeader: {
+                bold: true,
+                fontSize: 11,
+                color: 'black'
+            }
+        },
+        defaultStyle: {
+        	fontSize: 10
+            // alignment: 'justify'
+        }
+
+    };
+		pdfMake.createPdf(dd).open();
+	}},SO.ID);
+    }
+});
 	
-};	
-return dd;
- 
-	}
+	
+	
+	
+    
+
+}
 
 // @region namespaceDeclaration// @startlock
+	var button_saveSOLine = {};	// @button
+	var genProdOrder_btn = {};	// @button
 	var btn_print_so = {};	// @button
 	var documentEvent = {};	// @document
 	var id_btn_ro_clear = {};	// @button
@@ -94,11 +146,29 @@ return dd;
 
 // eventHandlers// @lock
 
+	button_saveSOLine.touchend = function button_saveSOLine_touchend (event)// @startlock
+	{// @endlock
+		// Add your code here
+	};// @lock
+
+	button_saveSOLine.click = function button_saveSOLine_click (event)// @startlock
+	{// @endlock
+		button_createSOLine.focus();s
+	};// @lock
+
+
+	
+	genProdOrder_btn.click = function genProdOrder_btn_click (event)// @startlock
+	{// @endlock
+		
+		var x = sources.salesOrder;
+	};// @lock
+
 	btn_print_so.click = function btn_print_so_click (event)// @startlock
 	{// @endlock
 		
-		var dd = preparePrint();
-		pdfMake.createPdf(dd).open();
+		prepareSOPrint();
+		
 	};// @lock
 
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
@@ -211,10 +281,14 @@ return dd;
 			query_str = query_str+(i!=0?'and ':'')+query_array[i];
 			
 		}
+		
 		sources.salesOrder.query(query_str);
 	};// @lock
 
 // @region eventManager// @startlock
+	WAF.addListener("button_saveSOLine", "touchend", button_saveSOLine.touchend, "WAF");
+	WAF.addListener("button_saveSOLine", "click", button_saveSOLine.click, "WAF");
+	WAF.addListener("genProdOrder_btn", "click", genProdOrder_btn.click, "WAF");
 	WAF.addListener("btn_print_so", "click", btn_print_so.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("id_btn_ro_clear", "click", id_btn_ro_clear.click, "WAF");
